921d279306af5079540c72258cce28e9d584d573
uskota6 <106528877+uskota6@users.noreply.github.com>
2023-11-17 16:15:47 +0530

    Add files via upload

---
+++ master/master/src/main/java/com/example/githubdemo/test1/Test1.java
@@ -0,0 +1,19 @@
+package com.example.githubdemo.test1;
+
+public class Test1 {
+    public static void  addTest1(){
+        System.out.println("test1");
+    }
+
+    public static void  addTest1ToBeCherryPicked(){
+        System.out.println("cherry pick ");
+    }
+
+    public static void  addTest2(){
+        System.out.println("commit1 ");
+    }
+
+    public static void  addTest3(){
+        System.out.println("commit2 ");
+    }
+}
